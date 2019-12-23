55c841556eb1c9ddcde19fd2093f9c7ec9d270a5
Кирилл <@192.168.0.101>
2019-12-23 13:12:40 +0500

    fixin

--- WebService/src/main/java/com/webapp/WebService/Controller.java
+++ WebService/src/main/java/com/webapp/WebService/Controller.java
@@ -98,6 +98,7 @@ public class Controller {
 		CallToInfromEnd();
 		return list;
 	}
+	@PostMapping("/upload/{geomtype}")
 	public void PostData (@RequestParam(name = "geom") String insertedGeom, @PathVariable String geomtype) {
 		CallToInfromStart();
 		ConnectToPostgres();
